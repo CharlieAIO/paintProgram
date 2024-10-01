68909d70ed97fdf5299ac84ecab9f1f4c7fafe32
CharlieAIO <charliebottomley15@gmail.com>
2024-10-01 10:32:19 +0100

    painting tool

--- PaintProgramProject/PaintProgramProject/Form1.Designer.cs
+++ PaintProgramProject/PaintProgramProject/Form1.Designer.cs
@@ -28,20 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(35, 504);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Clear";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlLight;
-            ClientSize = new Size(451, 256);
+            ClientSize = new Size(396, 537);
+            Controls.Add(button1);
             ForeColor = Color.Black;
+            DoubleBuffered = true;
             Name = "Form1";
             Text = "Form1";
+            Paint += Form1_Paint;
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            MouseUp += Form1_MouseUp;
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- PaintProgramProject/PaintProgramProject/Form1.cs
+++ PaintProgramProject/PaintProgramProject/Form1.cs
@@ -3,11 +3,46 @@ namespace PaintProgramProject
     public partial class Form1 : Form
     {
         Bitmap MyBitMap;
-        bool MouseDown = false;
+        bool isMouseDown;
         public Form1()
         {
             InitializeComponent();
-            MyBitMap = new Bitmap(Size.Width,Size.Height);
+            MyBitMap = new Bitmap(Size.Width, Size.Height);
+            isMouseDown = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMouseDown) return;
+
+            Graphics graphics = Graphics.FromImage(MyBitMap);
+            Pen redP = new Pen(Color.Red, 10);
+
+            graphics.DrawLine(redP, e.X, e.Y, e.X+1, e.Y+1);
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.DrawImageUnscaled(MyBitMap, 0, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = Graphics.FromImage(MyBitMap);
+            graphics.Clear(Color.Transparent);
+            Refresh();
         }
     }
 }
